e88f3acd0e384d412daff7f7a97bd27adf283b96
Ivan Andika <ivanandika@u.nus.edu>
2020-01-18 17:49:06 +0800

    add ajax post

--- contentscript.js
+++ contentscript.js
@@ -1,11 +1,46 @@
 main();
 
 function main(){
+
+
     let threadDetails = document.getElementsByClassName('rc-ThreadDetail');
-    let threadId = threadDetails[0].childNodes[0].getAttribute('id');
+    let threadId = threadDetails[0].childNodes[0].getAttribute('id').trim();
     console.log("The thread ID is ", threadId);
 
     let postArray = [];
+    // let rolesArray = []; //"Teaching Staff" or "Learner"
     let postContents = document.getElementsByClassName('rc-CML styled');
-    
+    //let profiles = document.getElementsByClassName('rc-ProfileArea');
+
+    let role = "";
+    let postContent = "";
+    for (let i = 0; i < postContents.length; i++) {
+        postContent = postContents[i].textContent;
+        console.log(postContent);
+        //let roleAttribute = profiles[i].childNodes[0].getAttribute('aria-label');
+        //roleAttribute = roleAttribute.slice(roleAttribute.indexOf("Role: ") + "Role: ".length);
+        //role = roleAttribute.slice(0, -1);
+        //console.log(role);
+        postArray.push(postContent);
+        //rolesArray.push(role);
+    } 
+
+    const url = "localhost:5000/predict";
+    let data = JSON.stringify(postArray);
+    function ajaxPost(element) {
+        $.ajax({
+            type:"POST",
+            url: url,
+            data: data,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            error: function() {
+                alert("Error");
+            },
+            success: function() {
+                alert("OK");
+            }
+        });
+    }
 }
+
